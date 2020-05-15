2e36f07a2106d9c275c356a9b004f551318f6585
Md.Atiquzzaman Soikat <atik.aiub@gmail.com>
2020-05-16 04:13:40 +0600

    updated header

--- functions/getCovidStats.js
+++ functions/getCovidStats.js
@@ -1,9 +1,15 @@
 const getData = require('../app/getData.js');
 const axios = require('axios');
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
 exports.handler = function(event, context, callback){
     const { httpMethod } = event
     if(httpMethod !== 'GET'){
        callback(null, {
+           headers,
            statusCode: 404,
            body: 'Not Found'
        })
