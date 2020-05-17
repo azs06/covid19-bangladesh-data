67a376ac36056a8faafe4c0218640c5151b3e690
Md.Atiquzzaman Soikat <atik.aiub@gmail.com>
2020-05-17 14:42:10 +0600

    added cors header

--- functions/getCovidStats.js
+++ functions/getCovidStats.js
@@ -1,21 +1,21 @@
 const getData = require('../app/getData.js');
 const axios = require('axios');
 const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "*",
 };
 exports.handler = function(event, context, callback){
     const { httpMethod } = event
     if(httpMethod !== 'GET'){
        callback(null, {
-           headers,
            statusCode: 404,
            body: 'Not Found'
        })
     }else{
         getData("https://iedcr.gov.bd/").then((response)=> {
             callback(null, {
+                headers,
                 statusCode: 200,
                 body: JSON.stringify({
                   data: response
